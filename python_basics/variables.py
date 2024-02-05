613cecffb119719f1a3f1d9e4a4462c57472f209
JBelem <89881288+Belem27@users.noreply.github.com>
2024-02-05 09:26:48 +0000

    variables

---
+++ python_basics/variables.py
@@ -0,0 +1,18 @@
+# variable = a container for a value. Behaves as the value that it contains
+
+# str
+first_name = "Jacob"
+last_name = "Belem"
+full_name = first_name + " " + last_name
+print("Hello "+full_name)
+print(type(full_name))
+
+# int
+age = 21
+age += 1
+print("Your age is: "+str(age))
+print(type(age))
+
+# bool
+height = 270.0
+print(5/3)
