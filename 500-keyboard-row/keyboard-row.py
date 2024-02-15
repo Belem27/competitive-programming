0446ad27cf866874dfb0517b4c30a19097194bfa
JBelem <89881288+Belem27@users.noreply.github.com>
2024-02-15 12:32:50 +0000

    Time: 18 ms (24.29%) | Memory: 11.8 MB (52.45%) - LeetSync

---
+++ 500-keyboard-row/keyboard-row.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findWords(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        keyboard_rows = {
+            'q': 1, 'w': 1, 'e': 1, 'r': 1, 't': 1, 'y': 1, 'u': 1, 'i': 1, 'o': 1, 'p': 1,
+            'a': 2, 's': 2, 'd': 2, 'f': 2, 'g': 2, 'h': 2, 'j': 2, 'k': 2, 'l': 2,
+            'z': 3, 'x': 3, 'c': 3, 'v': 3, 'b': 3, 'n': 3, 'm': 3
+        }
+        result = []
+        for word in words:
+            row = keyboard_rows[word[0].lower()]
+            if all(keyboard_rows[char.lower()] == row for char in word):
+                result.append(word)
+        return result
+            
