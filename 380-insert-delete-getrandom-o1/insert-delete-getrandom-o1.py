ed190be2c59797275105c95e84759a54c2ae9c39
JBelem <89881288+Belem27@users.noreply.github.com>
2024-02-16 19:12:15 +0000

    Time: 446 ms (49.84%) | Memory: 57.1 MB (93.52%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,8 +1,7 @@
 class RandomizedSet(object):
 
     def __init__(self):
-        self.hashmap = {}  # Dictionary to store values and their indices in the array
-        self.array = []    # List to store values
+        self.hashmap = set()
 
     def insert(self, val):
         """
@@ -11,8 +10,7 @@ class RandomizedSet(object):
         """
         if val in self.hashmap:
             return False
-        self.array.append(val)
-        self.hashmap[val] = len(self.array) - 1
+        self.hashmap.add(val)
         return True
 
     def remove(self, val):
@@ -22,19 +20,14 @@ class RandomizedSet(object):
         """
         if val not in self.hashmap:
             return False
-        index = self.hashmap[val]
-        last_val = self.array[-1]
-        self.array[index] = last_val
-        self.hashmap[last_val] = index
-        self.array.pop()
-        del self.hashmap[val]
+        self.hashmap.remove(val)
         return True
 
     def getRandom(self):
         """
         :rtype: int
         """
-        return random.choice(self.array)
+        return random.choice(list(self.hashmap))
 
 
 # Your RandomizedSet object will be instantiated and called as such:
