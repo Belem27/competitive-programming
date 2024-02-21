29283d26bc8013d057336580a9608510569f7271
JBelem <89881288+Belem27@users.noreply.github.com>
2024-02-21 15:38:52 +0000

    Time: 29 ms (92.14%) | Memory: 11.8 MB (87.14%) - LeetSync

---
+++ 829-subdomain-visit-count/subdomain-visit-count.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def subdomainVisits(self, cpdomains):
+        """
+        :type cpdomains: List[str]
+        :rtype: List[str]
+        """
+        domains = Counter()
+        
+        for cpdomain in cpdomains:
+            count_visits, domain = cpdomain.split()
+            subdomain_i = domain.find('.')
+            subdomain_j = domain.rfind('.')
+            
+            count_visits = int(count_visits)
+            domains[domain] += count_visits
+            domains[domain[subdomain_i + 1:]] += count_visits
+            
+            if (subdomain_i != subdomain_j):
+                domains[domain[subdomain_j + 1:]] += count_visits
+                
+        answer = []
+        for domain, count in domains.items():
+            answer.append(str(count) + ' ' + domain)
+            
+        return answer
+        
+
+            
