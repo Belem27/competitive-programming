d1aa6dc5eacc451f47188e4b7335936517ed5947
JBelem <89881288+Belem27@users.noreply.github.com>
2024-02-26 11:41:38 +0000

    Time: 35 ms (51.11%) | Memory: 12.5 MB (83.04%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -6,20 +6,12 @@ class Solution(object):
         """
         l, r = 0, len(s) - 1
         while l < r:
-            while l < r and not self.alphanumeric(s[l]):
+            while l < r and not s[l].isalnum():
                 l += 1
-            while l < r and not self.alphanumeric(s[r]):
+            while l < r and not s[r].isalnum():
                 r -= 1
             if s[l].lower() != s[r].lower():
                 return False
             l += 1
             r -= 1
         return True
-
-    #check if char is alphanumeric using ASCII
-    def alphanumeric(self, c):
-        return (
-            ord("A") <= ord(c) <= ord("Z")
-            or ord("a") <= ord(c) <= ord("z")
-            or ord("0") <= ord(c) <= ord("9")
-        )
