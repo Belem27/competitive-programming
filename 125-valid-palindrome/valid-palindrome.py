788942936606412229928e7959a8171e9dc7c603
JBelem <89881288+Belem27@users.noreply.github.com>
2024-02-26 12:50:14 +0000

    Time: 22 ms (91.27%) | Memory: 14.3 MB (38.57%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -5,4 +5,4 @@ class Solution(object):
         :rtype: bool
         """
         s = [c.lower() for c in s if c.isalnum()]
-        return all(s[i] == s[~i] for i in range((len(s)//2)))
+        return (s == s[::-1])
