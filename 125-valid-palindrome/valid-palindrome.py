e251212ec4aa951134394500e81bb5b0b57fdaa4
JBelem <89881288+Belem27@users.noreply.github.com>
2024-02-26 12:49:02 +0000

    Time: 36 ms (49.09%) | Memory: 14.5 MB (31.64%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,14 +4,5 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        l, r = 0, len(s) - 1
-        while l < r:
-            while l < r and not s[l].isalnum():
-                l += 1
-            while l < r and not s[r].isalnum():
-                r -= 1
-            if s[l].lower() != s[r].lower():
-                return False
-            l += 1
-            r -= 1
-        return True
+        s = [c.lower() for c in s if c.isalnum()]
+        return all(s[i] == s[~i] for i in range((len(s)//2)))
