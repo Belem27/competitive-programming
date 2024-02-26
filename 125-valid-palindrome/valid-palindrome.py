4651fce562a0b31afdcd59929b9f2fa3189eca9f
JBelem <89881288+Belem27@users.noreply.github.com>
2024-02-26 11:37:58 +0000

    Time: 48 ms (39.77%) | Memory: 12.2 MB (95.53%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,8 +4,22 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        v = "".join(s.split()).lower()
-        var = "".join(filter(str.isalnum, str(v)))
-        if var[::-1] == var:
-            return 1
-        return 0
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not self.alphanumeric(s[l]):
+                l += 1
+            while l < r and not self.alphanumeric(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
+
+    #check if char is alphanumeric using ASCII
+    def alphanumeric(self, c):
+        return (
+            ord("A") <= ord(c) <= ord("Z")
+            or ord("a") <= ord(c) <= ord("z")
+            or ord("0") <= ord(c) <= ord("9")
+        )
