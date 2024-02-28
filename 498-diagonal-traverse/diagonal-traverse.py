c73994e54b6b2384d50ec34a676d20872cb3319c
JBelem <89881288+Belem27@users.noreply.github.com>
2024-02-28 14:00:35 +0000

    Time: 145 ms (39.73%) | Memory: 16.2 MB (42.83%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.py
+++ 498-diagonal-traverse/diagonal-traverse.py
@@ -4,15 +4,13 @@ class Solution(object):
         :type mat: List[List[int]]
         :rtype: List[int]
         """
-        d = collections.defaultdict(list)
+        d = defaultdict(list)
         for i in range(len(mat)):
             for j in range(len(mat[0])):
                 d[i+j].append(mat[i][j])
         
-        print(d)
-        ans= []
+        ans = []
         for entry in d.items():
-            print(entry[1])
             if entry[0] % 2 == 0:
                 ans.extend(entry[1][::-1])
             else:
