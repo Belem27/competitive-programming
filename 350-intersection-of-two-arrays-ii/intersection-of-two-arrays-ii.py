275f75a44592585f91076e970434a79e40b2fa91
JBelem <89881288+Belem27@users.noreply.github.com>
2024-03-04 15:14:40 +0000

    Time: 24 ms (84.76%) | Memory: 11.7 MB (71.03%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -5,20 +5,15 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
+        hashmap = {}
         result = []
-        nums1.sort()
-        nums2.sort()
 
-        i, j = 0, 0
+        for num in nums1:
+            hashmap[num] = hashmap.get(num, 0) + 1
 
-        while i < len(nums1) and j < len(nums2):
-            if nums1[i] < nums2[j]:
-                i += 1
-            elif nums1[i] > nums2[j]:
-                j += 1
-            else:
-                result.append(nums1[i]) 
-                i += 1
-                j += 1
+        for num in nums2:
+            if num in hashmap and hashmap[num] > 0:
+                result.append(num)
+                hashmap[num] -= 1
         
         return result
