8277043fa299e647235d025886377b3033855cb1
JBelem <89881288+Belem27@users.noreply.github.com>
2024-03-10 07:01:16 +0000

    Time: 11 ms (92.25%) | Memory: 11.8 MB (19.32%) - LeetSync

---
+++ 75-sort-colors/sort-colors.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        zeros, ones, twos = [], [], []
+
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                zeros.append(0)
+            
+            if nums[i] == 1:
+                ones.append(1)
+
+            if nums[i] == 2:
+                twos.append(2)
+
+        nums*=0
+
+        nums.extend(zeros)
+        nums.extend(ones)
+        nums.extend(twos)
