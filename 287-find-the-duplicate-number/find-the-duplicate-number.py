747774b18b13c14f996d8ab2273381de8ef65d22
JBelem <89881288+Belem27@users.noreply.github.com>
2024-03-11 20:00:57 +0000

    Time: 498 ms (40.34%) | Memory: 23.6 MB (54.64%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -4,8 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        seen = set()
-        for num in nums:
-            if num in seen or seen.add(num):
-                return num 
+        first = nums[0]
+        second = nums[0]
+
+        while True:
+            first = nums[first]
+            second = nums[nums[second]]
+            if first == second:
+                break
         
+        first = nums[0]
+        while first != second:
+            first = nums[first]
+            second = nums[second]
+        
+        return first
