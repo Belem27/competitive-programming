82b2cf713e10c4cd7bc4fe7b4365c1ab267b43a0
JBelem <89881288+Belem27@users.noreply.github.com>
2024-03-14 16:44:17 +0000

    Time: 31 ms (45.07%) | Memory: 12.4 MB (33.75%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -5,15 +5,10 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        l, r = 0, len(nums) - 1
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
 
-        while l <= r:
-            mid = (l + r) // 2
-
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
-                l = mid + 1
-            else:
-                r = mid - 1
-        return l
+        nums.append(target)
+        nums.sort()
+        return nums.index(target)
