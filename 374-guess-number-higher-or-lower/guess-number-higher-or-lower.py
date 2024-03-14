42d0c17462db7faea61b1525902367d27da1c678
JBelem <89881288+Belem27@users.noreply.github.com>
2024-03-14 18:33:01 +0000

    Time: 14 ms (50.43%) | Memory: 11.6 MB (41.38%) - LeetSync

--- 374-guess-number-higher-or-lower/guess-number-higher-or-lower.py
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.py
@@ -14,9 +14,10 @@ class Solution(object):
         l, r = 1, n
 
         while l <= r:
-            if guess((l + r) // 2) == 0:
+            check = guess((l + r) // 2)
+            if check == 0:
                 return (l + r) // 2
-            elif guess((l + r) // 2) == 1:
+            elif check == 1:
                 l = ((l + r) // 2) + 1
             else:
                 r = ((l + r) // 2) - 1
