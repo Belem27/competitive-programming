2b2e7743ca76e4864da54d61fe9dafbfbb1e5d3b
JBelem <89881288+Belem27@users.noreply.github.com>
2024-03-18 16:59:41 +0000

    Time: 25 ms (35.15%) | Memory: 12.6 MB (34.45%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -4,17 +4,6 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        answer = []
-
-        for i in range(1, n + 1):
-            if i % 3 == 0 and i % 5 == 0:
-                answer.append("FizzBuzz")
-            elif i % 3 == 0:
-                answer.append("Fizz")
-            elif i % 5 == 0:
-                answer.append("Buzz")
-            else:
-                answer.append(str(i))
-        return answer
+        return ['Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i) for i in range(1, n + 1)]
 
         
