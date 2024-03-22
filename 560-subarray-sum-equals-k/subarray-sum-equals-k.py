e9080056add1437de3fc3c313f536e76467308e9
JBelem <89881288+Belem27@users.noreply.github.com>
2024-03-22 19:03:16 +0000

    Time: 223 ms (61.85%) | Memory: 14.2 MB (51.47%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        count = 0
+        cumulative_sum = 0
+        sum_count = {0: 1}
+        
+        for num in nums:
+            cumulative_sum += num
+            if cumulative_sum - k in sum_count:
+                count += sum_count[cumulative_sum - k]
+            sum_count[cumulative_sum] = sum_count.get(cumulative_sum, 0) + 1
+            
+        return count
