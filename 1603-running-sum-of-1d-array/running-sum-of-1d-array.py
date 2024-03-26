40b876d0695808a798aa87fbf316ccb7b895cd6c
JBelem <89881288+Belem27@users.noreply.github.com>
2024-03-26 18:06:55 +0000

    Time: 11 ms (98.96%) | Memory: 11.9 MB (40.14%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -4,12 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        n = len(nums)
-        running_sum = [0] * n  
-        current_sum = 0  
-        
-        for i in range(n):
-            current_sum += nums[i]
-            running_sum[i] = current_sum
-        
-        return running_sum
+        a=[]
+        s=0
+        for i in range(len(nums)):
+            s+=nums[i]
+            a.append(s)
+        return a
