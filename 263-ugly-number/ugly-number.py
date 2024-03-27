71e2e203e678d43f3f7d634ef0ed5d4f4122e845
JBelem <89881288+Belem27@users.noreply.github.com>
2024-03-27 14:04:41 +0000

    Time: 8 ms (95.42%) | Memory: 11.6 MB (78.47%) - LeetSync

--- 263-ugly-number/ugly-number.py
+++ 263-ugly-number/ugly-number.py
@@ -4,16 +4,14 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        while n >= 1:
+        while n > 1:
             if n % 2 == 0:
                 n //= 2
             elif n % 3 == 0:
                 n //= 3
             elif n % 5 == 0:
                 n //= 5
-            elif n == 1:
-                return True
             else:
-                return False
-        
-        return False
+                break
+                
+        return n == 1
