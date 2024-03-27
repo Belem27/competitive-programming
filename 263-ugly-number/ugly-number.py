5e97b7d68eacedac1de9c4d247d7610f098c4894
JBelem <89881288+Belem27@users.noreply.github.com>
2024-03-27 14:06:11 +0000

    Time: 13 ms (68.81%) | Memory: 11.7 MB (16.21%) - LeetSync

--- 263-ugly-number/ugly-number.py
+++ 263-ugly-number/ugly-number.py
@@ -6,12 +6,12 @@ class Solution(object):
         """
         while n > 1:
             if n % 2 == 0:
-                n /= 2
+                n //= 2
             elif n % 3 == 0:
-                n /= 3
+                n //= 3
             elif n % 5 == 0:
-                n /= 5
+                n //= 5
             else:
                 break
-
+                
         return n == 1
