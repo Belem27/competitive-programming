834ce2b40ef97f1de8eb8b14f69c834a82033bc8
JBelem <89881288+Belem27@users.noreply.github.com>
2024-04-01 19:24:41 +0000

    Time: 36 ms (29.06%) | Memory: 11.7 MB (93.19%) - LeetSync

--- 1547-destination-city/destination-city.py
+++ 1547-destination-city/destination-city.py
@@ -4,5 +4,13 @@ class Solution(object):
         :type paths: List[List[str]]
         :rtype: str
         """
+        path_count = {}
 
-        return "".join(city for city in [paths[i][1] for i in range(len(paths))] if city not in [paths[i][0] for i in range(len(paths))])
+        for path in paths:
+            path_a, path_b = path
+            path_count[path_a] = path_count.get(path_a, 0) + 1
+            path_count[path_b] = path_count.get(path_b, 0)
+
+        for city in path_count:
+            if path_count[city] == 0:
+                return city
