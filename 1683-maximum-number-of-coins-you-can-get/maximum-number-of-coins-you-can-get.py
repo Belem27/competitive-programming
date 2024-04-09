3814182e6215b4c4e3b9f8343c8cec42921ea1f0
JBelem <89881288+Belem27@users.noreply.github.com>
2024-04-09 23:51:43 +0000

    Time: 495 ms (45.55%) | Memory: 21.4 MB (76.24%) - LeetSync

--- 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.py
+++ 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.py
@@ -7,8 +7,7 @@ class Solution(object):
         piles.sort(reverse=True)
         ans = 0
 
-        for _ in range((len(piles)//3)):
-            piles.remove(piles[0])
-            ans += piles.pop(0)
+        for i in range((len(piles)//3)):
+            ans += piles[(i*2)+1]
         
         return ans
