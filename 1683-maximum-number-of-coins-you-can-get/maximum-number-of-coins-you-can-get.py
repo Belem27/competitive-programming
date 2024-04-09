2a021e73ca7bb8898919c8f84ae14c35e4961640
JBelem <89881288+Belem27@users.noreply.github.com>
2024-04-09 23:53:19 +0000

    Time: 476 ms (78.71%) | Memory: 21.6 MB (21.78%) - LeetSync

--- 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.py
+++ 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.py
@@ -4,10 +4,5 @@ class Solution(object):
         :type piles: List[int]
         :rtype: int
         """
-        piles.sort(reverse=True)
-        ans = 0
-
-        for i in range((len(piles)//3)):
-            ans += piles[(i*2)+1]
-        
-        return ans
+        piles.sort()
+        return sum(itertools.islice(piles, len(piles)//3, len(piles), 2))
