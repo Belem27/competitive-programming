294ee06e64143c0c3cfe9bc9b3c283ef44fbfe19
JBelem <89881288+Belem27@users.noreply.github.com>
2024-04-10 02:16:03 +0000

    Time: 24 ms (45.71%) | Memory: 11.7 MB (40.00%) - LeetSync

---
+++ 1009-pancake-sorting/pancake-sorting.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def pancakeSort(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[int]
+        """
+        def reverse(l, begin, end):
+            for i in xrange((end-begin) // 2):
+                l[begin+i], l[end-1-i] = l[end-1-i], l[begin+i]
+
+        result = []
+        for n in reversed(xrange(1, len(arr)+1)):
+            i = arr.index(n)
+            reverse(arr, 0, i+1)
+            result.append(i+1)
+            reverse(arr, 0, n)
+            result.append(n)
+        return result
