51efa7789bdcb0bb583c1f2475ca400a562ec474
JBelem <89881288+Belem27@users.noreply.github.com>
2024-04-10 00:27:34 +0000

    Time: 739 ms (10.40%) | Memory: 21.3 MB (92.57%) - LeetSync

--- 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.py
+++ 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.py
@@ -4,5 +4,15 @@ class Solution(object):
         :type piles: List[int]
         :rtype: int
         """
-        piles.sort()
-        return sum(itertools.islice(piles, len(piles)//3, len(piles), 2))
+        piles.sort(reverse=True)
+        print(piles)
+        
+        i = 1        
+        coins = 0
+        
+        while i < len(piles) - (len(piles) //3):
+            print(i)
+            coins += piles[i]
+            i += 2
+            
+        return coins
