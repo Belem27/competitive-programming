73b74ede211d62768c2295322e1ed529551f0ab3
JBelem <89881288+Belem27@users.noreply.github.com>
2024-04-15 00:12:03 +0000

    Created 'Smallest Value of the Rearranged Number' by A2SV Hub

---
+++ 15-Apr-2024/Smallest Value of the Rearranged Number 123363.py
@@ -0,0 +1,31 @@
+# Problem: Smallest Value of the Rearranged Number - https://leetcode.com/problems/smallest-value-of-the-rearranged-number/description/
+
+class Solution(object):
+    def smallestNumber(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        ans = []
+
+        if num > 0:
+            num = sorted([int(n) for n in str(num)])
+            i = 1
+            while num[0] == 0:
+                num[0], num[i] = num[i], num[0]
+                i += 1
+
+            for n in num:
+                ans.append(n)
+
+        elif num < 0:
+            nl = str(num)
+            num = [int(n) for n in str(nl[1:])]
+            num.sort(reverse=True)
+            for n in num:
+                ans.append(n)
+            ans.insert(0, "-")
+        else:
+            return num
+
+        return int("".join(map(str, ans)))
