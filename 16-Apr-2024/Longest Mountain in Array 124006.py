364e9ba20a4bc0f649804a2d180a516204811377
JBelem <89881288+Belem27@users.noreply.github.com>
2024-04-16 07:45:47 +0000

    Created 'Longest Mountain in Array' by A2SV Hub

---
+++ 16-Apr-2024/Longest Mountain in Array 124006.py
@@ -0,0 +1,24 @@
+# Problem: Longest Mountain in Array - https://leetcode.com/problems/longest-mountain-in-array/
+
+class Solution(object):
+    def longestMountain(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        n = len(arr)
+        max_length = 0
+        
+        for i in range(1, n - 1):
+            if arr[i] > arr[i - 1] and arr[i] > arr[i + 1]:
+                left, right = i - 1, i + 1
+                
+                while left > 0 and arr[left] > arr[left - 1]:
+                    left -= 1
+                
+                while right < n - 1 and arr[right] > arr[right + 1]:
+                    right += 1
+                
+                max_length = max(max_length, right - left + 1)
+        
+        return max_length
