b133360122df84a9edc8e1b602c720ecf280e4d2
JBelem <89881288+Belem27@users.noreply.github.com>
2024-04-18 14:40:50 +0000

    Time: 33 ms (64.56%) | Memory: 18.5 MB (97.41%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -12,15 +12,14 @@ class Solution(object):
         """
         if not head or not head.next:
             return False
-        
+
         slow = head
         fast = head.next
-        
+
         while slow != fast:
             if not fast or not fast.next:
                 return False
-            
             slow = slow.next
             fast = fast.next.next
-        
+            
         return True
