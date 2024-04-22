6116143145453a6d58922a08e6ae7e9e5b4553e5
JBelem <89881288+Belem27@users.noreply.github.com>
2024-04-22 18:23:30 +0000

    Created 'Valid parenthesis' by A2SV Hub

---
+++ 22-Apr-2024/Valid parenthesis 130380.py
@@ -0,0 +1,18 @@
+# Problem: Valid parenthesis - https://leetcode.com/problems/valid-parentheses/
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        bracket_map = {")":"(","}":"{","]":"["}
+
+        for bracket in s:
+            if bracket in bracket_map.values():
+                stack.append(bracket)
+            elif bracket in bracket_map:
+                if not stack or stack.pop() != bracket_map[bracket]:
+                    return False
+        return not stack
