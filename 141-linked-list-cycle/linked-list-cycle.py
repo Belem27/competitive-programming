f3127c04e0aec363ffbffec1d933e1fd0ea87a84
JBelem <89881288+Belem27@users.noreply.github.com>
2024-04-22 12:15:00 +0000

    Time: 35 ms (51.57%) | Memory: 19.3 MB (11.59%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -10,16 +10,4 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
-        if not head or not head.next:
-            return False
-
-        slow = head
-        fast = head.next
-
-        while slow != fast:
-            if not fast or not fast.next:
-                return False
-            slow = slow.next
-            fast = fast.next.next
-            
-        return True
+        return ListNode.has_cycle(head)
