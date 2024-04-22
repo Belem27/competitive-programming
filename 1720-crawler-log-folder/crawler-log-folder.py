df9f4e74f3b7453d50f2a171ae2d440d4b0b5c53
JBelem <89881288+Belem27@users.noreply.github.com>
2024-04-22 19:44:22 +0000

    Time: 12 ms (98.75%) | Memory: 12 MB (15.00%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def minOperations(self, logs):
+        """
+        :type logs: List[str]
+        :rtype: int
+        """
+        stack = []
+
+        for operation in logs:
+            if operation == "../" and stack:
+                stack.pop()
+            elif operation != "./" and operation != "../":
+                stack.append(operation)
+        
+        print(stack)
+        return len(stack)
