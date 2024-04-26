fe3e19d4e7b3d0a5dbed6b84b1515f085703d075
JBelem <89881288+Belem27@users.noreply.github.com>
2024-04-26 16:50:33 +0000

    Created 'Union Of Two Arrays' by A2SV Hub

---
+++ 26-Apr-2024/Union Of Two Arrays 134540.py
@@ -0,0 +1,29 @@
+# Problem: Union Of Two Arrays - https://practice.geeksforgeeks.org/problems/union-of-two-arrays3538/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab
+
+#User function Template for python3
+
+class Solution:    
+    #Function to return the count of number of elements in union of two arrays.
+    def doUnion(self,a,n,b,m):
+        
+        #code here
+        a.extend(b)
+        return len(set(a))
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        
+        n,m=[int(x) for x in input().strip().split()]
+        
+        a=[int(x) for x in input().strip().split()]
+        b=[int(x) for x in input().strip().split()]
+        ob=Solution()
+        
+        print(ob.doUnion(a,n,b,m))
+# } Driver Code Ends
