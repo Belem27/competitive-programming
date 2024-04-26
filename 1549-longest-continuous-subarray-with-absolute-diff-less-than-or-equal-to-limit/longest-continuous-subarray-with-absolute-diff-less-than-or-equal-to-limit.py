857145714ae26bc4f41870fc95749ac398684cfc
JBelem <89881288+Belem27@users.noreply.github.com>
2024-04-26 16:31:41 +0000

    Time: 434 ms (96.23%) | Memory: 19.5 MB (75.47%) - LeetSync

---
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def longestSubarray(self, nums, limit):
+        """
+        :type nums: List[int]
+        :type limit: int
+        :rtype: int
+        """
+        max_dq, min_dq = deque(), deque()
+        left = 0
+        for right, num in enumerate(nums):
+            while max_dq and nums[max_dq[-1]] <= num:
+                max_dq.pop()
+            max_dq.append(right)
+            while min_dq and nums[min_dq[-1]] >= num:
+                min_dq.pop()
+            min_dq.append(right)
+            if nums[max_dq[0]]-nums[min_dq[0]] > limit:
+                if max_dq[0] == left:
+                    max_dq.popleft()
+                if min_dq[0] == left:
+                    min_dq.popleft()
+                left += 1 
+        return len(nums)-left
