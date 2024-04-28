fe1e8a16fdb87dcf4a8560d05e5c248489f9a2d4
JBelem <89881288+Belem27@users.noreply.github.com>
2024-04-28 23:04:19 +0000

    Created 'Merge Strings Alternately' by A2SV Hub

---
+++ 28-Apr-2024/Merge Strings Alternately 135783.py
@@ -0,0 +1,25 @@
+# Problem: Merge Strings Alternately - https://leetcode.com/problems/merge-strings-alternately/
+
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        result = []
+        l1, l2 = len(word1), len(word2)
+        diff = l1 - l2
+        i, j = 0, 0
+
+        while i < l1 and j < l2:
+            result.append(word1[i])
+            result.append(word2[j])
+
+            i += 1
+            j += 1
+        
+        result.append(word1[i:])
+        result.append(word2[i:])
+
+        return ''.join(result)
