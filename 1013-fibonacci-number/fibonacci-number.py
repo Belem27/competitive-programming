ecf3225d905885902dd5dfc066e96183ec0e3365
JBelem <89881288+Belem27@users.noreply.github.com>
2024-04-30 23:51:14 +0000

    Time: 16 ms (60.09%) | Memory: 11.5 MB (73.69%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -4,13 +4,7 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n <= 1:
-            return n
-
-        fib_num = [0] * (n + 1)
-        fib_num[1] = 1
-
-        for i in range(2, n + 1):
-            fib_num[i] = fib_num[i - 1] + fib_num[i - 2]
-        
-        return fib_num[n]
+        prev, current = 0, 1
+        for i in xrange(n):
+            prev, current = current, prev + current,
+        return prev
