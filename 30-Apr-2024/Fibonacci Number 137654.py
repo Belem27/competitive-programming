632add6c2b6d7e321939f10c122f543d0487a87c
JBelem <89881288+Belem27@users.noreply.github.com>
2024-04-30 23:51:14 +0000

    Created 'Fibonacci Number' by A2SV Hub

---
+++ 30-Apr-2024/Fibonacci Number 137654.py
@@ -0,0 +1,12 @@
+# Problem: Fibonacci Number - https://leetcode.com/problems/fibonacci-number/
+
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        prev, current = 0, 1
+        for i in xrange(n):
+            prev, current = current, prev + current,
+        return prev
