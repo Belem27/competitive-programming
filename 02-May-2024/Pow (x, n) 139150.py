d208c205d4322c7fa023298e536bbc483917b1fe
JBelem <89881288+Belem27@users.noreply.github.com>
2024-05-02 13:42:39 +0000

    Created 'Pow (x, n)' by A2SV Hub

---
+++ 02-May-2024/Pow (x, n) 139150.py
@@ -0,0 +1,10 @@
+# Problem: Pow (x, n) - https://leetcode.com/problems/powx-n/
+
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        return x ** n
