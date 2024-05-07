3f620978b91f01d4317476796f4ca3bac5b8936b
JBelem <89881288+Belem27@users.noreply.github.com>
2024-05-07 12:15:33 +0000

    Created 'A - FIA Heist' by A2SV Hub

---
+++ A2SV Remote G5 Contest 12 07-May-2024/A - FIA Heist 141980.py
@@ -0,0 +1,15 @@
+# Problem: A - FIA Heist - https://codeforces.com/gym/520098/problem/A
+
+def decode_intercepted_string(s):
+    stack = []
+    for char in s:
+        if char == '-' and stack and stack.pop() == "<":
+            if stack:
+                stack.pop()
+        else:
+            stack.append(char)
+    return ''.join(stack)
+
+intercepted_string = input().strip()
+
+print(decode_intercepted_string(intercepted_string))
