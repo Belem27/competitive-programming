fb7ed403d7dc828c29ec28a2d3a00efb39564f06
JBelem <89881288+Belem27@users.noreply.github.com>
2024-05-07 13:10:20 +0000

    Created 'E - Allison's Game' by A2SV Hub

---
+++ A2SV Remote G5 Contest 12 07-May-2024/E - Allison's Game 142016.py
@@ -0,0 +1,23 @@
+# Problem: E - Allison's Game - https://codeforces.com/gym/520098/problem/E
+
+N = int(input())
+heights = list(map(int, input().split()))
+
+stack = []
+max_area = 0
+
+for i in range(N):
+    while stack and heights[stack[-1]] >= heights[i]:
+        height = heights[stack.pop()]
+        width = i if not stack else i - stack[-1] - 1
+        side = min(height, width)
+        max_area = max(max_area, side)
+    stack.append(i)
+
+while stack:
+    height = heights[stack.pop()]
+    width = N if not stack else N - stack[-1] - 1
+    side = min(height, width)
+    max_area = max(max_area, side)
+
+print(max_area)
