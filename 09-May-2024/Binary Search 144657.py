0bac709afefb2c8fe17321a563ca32f5c5e10f84
JBelem <89881288+Belem27@users.noreply.github.com>
2024-05-09 18:37:35 +0000

    Created 'Binary Search' by A2SV Hub

---
+++ 09-May-2024/Binary Search 144657.py
@@ -0,0 +1,19 @@
+# Problem: Binary Search - https://leetcode.com/problems/binary-search/description/
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            mid = low + (high - low) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return -1
