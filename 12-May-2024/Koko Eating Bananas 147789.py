6b54bae54b7c5b506a630bde3df6899c929060d5
JBelem <89881288+Belem27@users.noreply.github.com>
2024-05-12 23:40:01 +0000

    Created 'Koko Eating Bananas' by A2SV Hub

---
+++ 12-May-2024/Koko Eating Bananas 147789.py
@@ -0,0 +1,20 @@
+# Problem: Koko Eating Bananas - https://leetcode.com/problems/koko-eating-bananas/
+
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        """
+        :type piles: List[int]
+        :type h: int
+        :rtype: int
+        """
+        def possible(piles, h, K):
+            return sum((pile-1)//K+1 for pile in piles) <= h
+
+        left, right = 1, max(piles)
+        while left <= right:
+            mid = left + (right-left)//2
+            if possible(piles, h, mid):
+                right = mid-1
+            else:
+                left = mid+1
+        return left
