7e87fe58901c661d84712980b78c19a07de5370a
JBelem <89881288+Belem27@users.noreply.github.com>
2024-05-13 20:55:18 +0000

    Time: 18 ms (82.87%) | Memory: 11.8 MB (89.32%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -4,15 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        left, right = 0, len(nums)
-        target = nums[-1]
-
-        while left < right:
-            mid = left + (right - left) // 2
-
-            if nums[mid] <= target:
-                right = mid
+        l, r = 0, len(nums) - 1
+        res = nums[0]
+        while l <= r:
+            if nums[l] < nums[r]:
+                res = min(res, nums[l])
+                break
+            m = l + (r - l) //2
+            res = min(res, nums[l])
+            if nums[l] <= nums[m]:
+                l = m + 1
             else:
-                left = mid + 1
-
-        return nums[left]
+                r = m
+        return res
