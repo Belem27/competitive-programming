66b9738e09f3e1fabeb06e9e2c263f76dc2bd684
JBelem <89881288+Belem27@users.noreply.github.com>
2024-05-13 20:37:19 +0000

    Created 'H-Index II' by A2SV Hub

---
+++ 13-May-2024/H-Index II 148403.py
@@ -0,0 +1,17 @@
+# Problem: H-Index II - https://leetcode.com/problems/h-index-ii/description/
+
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        n = len(citations)
+        left, right = 0, n - 1
+        while left <= right:
+            mid = (left + right) / 2
+            if citations[mid] >= n - mid:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return n - left
