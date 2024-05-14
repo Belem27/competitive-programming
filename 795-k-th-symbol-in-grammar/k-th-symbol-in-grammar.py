10f39ea9819fc8e26e713c350f73b6cd885c3e6b
JBelem <89881288+Belem27@users.noreply.github.com>
2024-05-14 21:38:02 +0000

    Time: 7 ms (93.67%) | Memory: 11.6 MB (35.44%) - LeetSync

---
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def kthGrammar(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        def bitCount(n):
+            result = 0
+            while n:
+                n &= n - 1
+                result += 1
+            return result
+
+        return bitCount(k-1) % 2
