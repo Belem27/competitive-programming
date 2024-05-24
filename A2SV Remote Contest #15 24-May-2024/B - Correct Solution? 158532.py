fcb7039a9e33b38e27bd53216638f60511752def
JBelem <89881288+Belem27@users.noreply.github.com>
2024-05-24 13:37:43 +0000

    Created 'B - Correct Solution?' by A2SV Hub

---
+++ A2SV Remote Contest #15 24-May-2024/B - Correct Solution? 158532.py
@@ -0,0 +1,13 @@
+# Problem: B - Correct Solution? - https://codeforces.com/gym/524150/problem/B
+
+alice = sorted(input())
+bob = list(input())
+i = 0
+n = len(alice)-1
+while i < n and alice[i] == "0":
+    i += 1
+alice[0], alice[i] = alice[i], alice[0]
+if alice == bob:
+    print("OK")
+else:
+    print("WRONG_ANSWER")
