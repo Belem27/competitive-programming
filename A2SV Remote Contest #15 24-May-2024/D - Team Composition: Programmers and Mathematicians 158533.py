bda73af902d7211b001fa7c9b8cbc58457030b97
JBelem <89881288+Belem27@users.noreply.github.com>
2024-05-24 13:37:48 +0000

    Created 'D - Team Composition: Programmers and Mathematicians' by A2SV Hub

---
+++ A2SV Remote Contest #15 24-May-2024/D - Team Composition: Programmers and Mathematicians 158533.py
@@ -0,0 +1,11 @@
+# Problem: D - Team Composition: Programmers and Mathematicians - https://codeforces.com/gym/524150/problem/D
+
+def max_teams(t, cases):
+    res = []
+    for case in cases:
+        a, b = case
+        res.append(min((a + b ) //4, min(a, b)))
+    return res
+t = int(input())
+for ans in max_teams(t, [tuple(map(int, input().strip().split())) for _ in range(t)]):
+    print(ans)
