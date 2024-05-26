f05892b352b87815787045dd6ee0273600085307
JBelem <89881288+Belem27@users.noreply.github.com>
2024-05-26 22:18:19 +0000

    Created 'Shortest Subarray with Sum at Least K' by A2SV Hub

---
+++ 26-May-2024/Shortest Subarray with Sum at Least K 159983.py
@@ -0,0 +1,28 @@
+# Problem: Shortest Subarray with Sum at Least K - https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/
+
+class Solution(object):
+    def shortestSubarray(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        n = len(nums)
+        prefix_sums = [0] * (n + 1)
+        
+        for i in range(n):
+            prefix_sums[i + 1] = prefix_sums[i] + nums[i]
+        
+        dq = deque()
+        min_length = float('inf')
+        
+        for i in range(n + 1):
+            while dq and prefix_sums[i] - prefix_sums[dq[0]] >= k:
+                min_length = min(min_length, i - dq.popleft())
+            
+            while dq and prefix_sums[i] <= prefix_sums[dq[-1]]:
+                dq.pop()
+            
+            dq.append(i)
+        
+        return min_length if min_length != float('inf') else -1
