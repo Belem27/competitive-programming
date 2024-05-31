b7acfc8ac525916ff636e0ae95eb5a42746101a1
JBelem <89881288+Belem27@users.noreply.github.com>
2024-05-31 22:38:10 +0000

    Created 'Word Ladder' by A2SV Hub

---
+++ 31-May-2024/Word Ladder 162916.py
@@ -0,0 +1,29 @@
+# Problem: Word Ladder - https://leetcode.com/problems/word-ladder/
+
+class Solution(object):
+    def ladderLength(self, beginWord, endWord, wordList):
+        """
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: List[str]
+        :rtype: int
+        """
+        lookup = set(wordList)
+        if endWord not in lookup:
+            return 0
+        ladder = 2
+        q = [beginWord]
+        while q:
+            new_q = []
+            for word in q:
+                for i in xrange(len(word)):
+                    for j in ascii_lowercase:
+                        new_word = word[:i] + j + word[i+1:]
+                        if new_word == endWord:
+                            return ladder
+                        if new_word in lookup:
+                            lookup.remove(new_word)
+                            new_q.append(new_word)
+            q = new_q
+            ladder += 1
+        return 0
