0d9159a3274da93afd80faa6656f3b8d7e3c230a
JBelem <89881288+Belem27@users.noreply.github.com>
2024-06-08 23:16:53 +0000

    Created 'B - Petr and a Combination Lock' by A2SV Hub

---
+++ A2SV Remote G5 Contest 18 08-Jun-2024/B - Petr and a Combination Lock 166058.py
@@ -0,0 +1,8 @@
+# Problem: B - Petr and a Combination Lock - https://codeforces.com/gym/528793/problem/B
+
+sums = {0}
+for _ in range(int(input())):
+    a = int(input())
+    sums = set((a + x) % 360 for x in sums) | set((a - x) % 360 for x in sums)
+ 
+print("YES" if 0 in sums else "NO")
