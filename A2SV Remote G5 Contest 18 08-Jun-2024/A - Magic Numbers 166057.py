100b5539f36823967fda1b934ab5dc9f61c914ed
JBelem <89881288+Belem27@users.noreply.github.com>
2024-06-08 23:16:28 +0000

    Created 'A - Magic Numbers' by A2SV Hub

---
+++ A2SV Remote G5 Contest 18 08-Jun-2024/A - Magic Numbers 166057.py
@@ -0,0 +1,10 @@
+# Problem: A - Magic Numbers - https://codeforces.com/gym/528793/problem/A
+
+n = input()
+a = n.count("144")
+b = n.count("14")
+c = n.count("1")
+if len(n) - (a+b+c) == 0:
+    print("YES")
+else:
+    print("NO")
