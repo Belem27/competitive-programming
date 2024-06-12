385516c9aac5e0e664b1b980809170d523d87075
JBelem <89881288+Belem27@users.noreply.github.com>
2024-06-12 23:05:59 +0000

    Created 'Balance a binary search tree' by A2SV Hub

---
+++ 12-Jun-2024/Balance a binary search tree 167608.py
@@ -0,0 +1,33 @@
+# Problem: Balance a binary search tree - https://leetcode.com/problems/balance-a-binary-search-tree/
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def balanceBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        def inorderTraversalHelper(node, arr):
+            if not node:
+                return
+            inorderTraversalHelper(node.left, arr)
+            arr.append(node.val)
+            inorderTraversalHelper(node.right, arr)
+        
+        def sortedArrayToBstHelper(arr, i, j):
+            if i >= j:
+                return None
+            mid = i + (j-i)//2
+            node = TreeNode(arr[mid])
+            node.left = sortedArrayToBstHelper(arr, i, mid)
+            node.right = sortedArrayToBstHelper(arr, mid+1, j)
+            return node
+        
+        arr = []
+        inorderTraversalHelper(root, arr)
+        return sortedArrayToBstHelper(arr, 0, len(arr))
