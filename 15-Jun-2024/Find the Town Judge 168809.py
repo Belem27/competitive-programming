4518521035d107d13394ff7eebbe2873a2bd66bc
JBelem <89881288+Belem27@users.noreply.github.com>
2024-06-15 23:20:04 +0000

    Created 'Find the Town Judge' by A2SV Hub

---
+++ 15-Jun-2024/Find the Town Judge 168809.py
@@ -0,0 +1,17 @@
+# Problem: Find the Town Judge - https://leetcode.com/problems/find-the-town-judge/
+
+class Solution(object):
+    def findJudge(self, n, trust):
+        """
+        :type n: int
+        :type trust: List[List[int]]
+        :rtype: int
+        """
+        degrees = [0]*n
+        for i, j in trust:
+            degrees[i-1] -= 1
+            degrees[j-1] += 1
+        for i in xrange(len(degrees)):
+            if degrees[i] == n-1:
+                return i+1
+        return -1
