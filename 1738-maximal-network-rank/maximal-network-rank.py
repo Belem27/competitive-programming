6582fd384950e3eafb738b53a0b70cd61db31259
JBelem <89881288+Belem27@users.noreply.github.com>
2024-06-21 22:27:08 +0000

    Time: 214 ms (98.18%) | Memory: 13.3 MB (43.64%) - LeetSync

--- 1738-maximal-network-rank/maximal-network-rank.py
+++ 1738-maximal-network-rank/maximal-network-rank.py
@@ -5,26 +5,6 @@ class Solution(object):
         :type roads: List[List[int]]
         :rtype: int
         """
-        MAX_N = 100
-        MAX_NUM = MAX_N-1
-        def counting_sort(arr, key=lambda x:x, reverse=False):  # Time: O(n), Space: O(n)
-            count = [0]*(MAX_NUM+1)
-            for x in arr:
-                count[key(x)] += 1
-            for i in xrange(1, len(count)):
-                count[i] += count[i-1]
-            result = [0]*len(arr)
-            if not reverse:
-                for x in reversed(arr):  # stable sort
-                    count[key(x)] -= 1
-                    result[count[key(x)]] = x
-            else:
-                for x in arr:  # stable sort
-                    count[key(x)] -= 1
-                    result[count[key(x)]] = x
-                result.reverse()
-            return result
-
         degree = [0]*n
         adj = collections.defaultdict(set)
         for a, b in roads:
@@ -32,7 +12,8 @@ class Solution(object):
             degree[b] += 1
             adj[a].add(b)
             adj[b].add(a)
-        sorted_idx = counting_sort(xrange(n), key=lambda x:degree[x], reverse=True)
+        sorted_idx = range(n)
+        sorted_idx.sort(key=lambda x:-degree[x])
         m = 2
         while m < n:
             if degree[sorted_idx[m]] != degree[sorted_idx[1]]:
