29a0a98226232582ebc11dffbc3f791e6f42e240
JBelem <89881288+Belem27@users.noreply.github.com>
2024-06-24 12:12:59 +0000

    Created 'Number of Islands' by A2SV Hub

---
+++ 24-Jun-2024/Number of Islands 172100.py
@@ -0,0 +1,31 @@
+# Problem: Number of Islands - https://leetcode.com/problems/number-of-islands/
+
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+        island = 0
+
+        def bfs(r, c):
+            q = collections.deque()
+            q.append((r, c))
+            visited.add((r, c))
+            while q:
+                row, col = q.popleft()
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+                for dr, dc in directions:
+                    r, c = dr + row, dc + col
+                    if r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r, c) not in visited:
+                        visited.add((r, c))
+                        q.append((r, c))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    bfs(r, c)
+                    island += 1
+        return island
