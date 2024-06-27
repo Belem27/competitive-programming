ec03aec081b53f9c6c5c5a67b2e79324796d120d
JBelem <89881288+Belem27@users.noreply.github.com>
2024-06-27 23:02:57 +0000

    Created 'Open the Lock' by A2SV Hub

---
+++ 27-Jun-2024/Open the Lock 173545.py
@@ -0,0 +1,33 @@
+# Problem: Open the Lock - https://leetcode.com/problems/open-the-lock/
+
+class Solution(object):
+    def openLock(self, deadends, target):
+        """
+        :type deadends: List[str]
+        :type target: str
+        :rtype: int
+        """
+        if "0000" in deadends:
+            return -1
+        
+        def children(lock):
+            res = []
+            for i in range(4):
+                digit = str((int(lock[i]) +1)%10)
+                res.append(lock[:i] + digit + lock[i+1:])
+                digit = str((int(lock[i]) -1)%10)
+                res.append(lock[:i] + digit + lock[i+1:])
+            return res
+            
+        visit = set(deadends)
+        q = deque()
+        q.append(["0000", 0])
+        while q:
+            lock, turns = q.popleft()
+            if lock == target:
+                return turns
+            for child in children(lock):
+                if child not in visit:
+                    visit.add(child)
+                    q.append([child, turns+1])
+        return -1
