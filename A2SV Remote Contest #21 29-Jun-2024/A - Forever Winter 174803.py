4a4c83ad158d3d6e75880c0386b10b93cd44d744
JBelem <89881288+Belem27@users.noreply.github.com>
2024-06-29 21:59:01 +0000

    Created 'A - Forever Winter' by A2SV Hub

---
+++ A2SV Remote Contest #21 29-Jun-2024/A - Forever Winter 174803.py
@@ -0,0 +1,16 @@
+# Problem: A - Forever Winter - https://codeforces.com/gym/532332/problem/A
+
+for i in range(int(input())):
+    n, m = map(int, input().split())
+    g = [[] for j in range(n)]
+    for j in range(m):
+        v, u = map(int, input().split())
+        g[v-1].append(u-1)
+        g[u-1].append(v-1)
+    q = {}
+    for j in range(n):
+        q[len(g[j])] = q.get(len(g[j]), 0) + 1
+    z = q[1]
+    x = n-z-1
+    y = z//x
+    print(x, y)
