03471fcc38861b67591dc106c014164ee1a43ebf
JBelem <89881288+Belem27@users.noreply.github.com>
2024-06-29 21:59:21 +0000

    Created 'B - New Year Transportation' by A2SV Hub

---
+++ A2SV Remote Contest #21 29-Jun-2024/B - New Year Transportation 174805.py
@@ -0,0 +1,14 @@
+# Problem: B - New Year Transportation - https://codeforces.com/gym/532332/problem/B
+
+n, t = map(int, input().split())
+a = list(map(int, input().split()))
+
+c = 1
+
+while c < t:
+    c += a[c-1]
+
+if t == c:
+    print("YES")
+else:
+    print("NO")
