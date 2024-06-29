fd2ab9d903b5e81f98848062cf0370bf6a7c562c
JBelem <89881288+Belem27@users.noreply.github.com>
2024-06-29 21:59:08 +0000

    Created 'A - Forever Winter' by A2SV Hub

---
+++ A2SV Remote Contest #21 29-Jun-2024/A - Forever Winter 174804.py
@@ -0,0 +1,21 @@
+# Problem: A - Forever Winter - https://codeforces.com/gym/532332/problem/A
+
+from collections import Counter
+
+
+def solve():
+    n, m = map(int, input().split())
+
+    g = [0 for _ in range(n)]
+
+    for _ in range(m):
+        u, v = map(int, input().split())
+        g[u - 1] += 1
+        g[v - 1] += 1
+
+    cnt = Counter(g)
+    print(n - cnt[1] - 1, cnt[1]//(n - cnt[1] - 1))
+
+
+for i in range(int(input())):
+    solve()
