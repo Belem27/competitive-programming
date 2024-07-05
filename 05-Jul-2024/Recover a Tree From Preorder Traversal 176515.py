f53055270b1af4f4b01c5e5067ad3de897c7d0fb
JBelem <89881288+Belem27@users.noreply.github.com>
2024-07-05 00:38:47 +0000

    Created 'Recover a Tree From Preorder Traversal' by A2SV Hub

---
+++ 05-Jul-2024/Recover a Tree From Preorder Traversal 176515.py
@@ -0,0 +1,36 @@
+# Problem: Recover a Tree From Preorder Traversal - https://leetcode.com/problems/recover-a-tree-from-preorder-traversal/
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def recoverFromPreorder(self, traversal):
+        """
+        :type traversal: str
+        :rtype: TreeNode
+        """
+        S = traversal
+        i = 0
+        stack = []
+        while i < len(S):
+            level = 0
+            while i < len(S) and S[i] == '-':
+                level += 1
+                i += 1
+            while len(stack) > level:
+                stack.pop()
+            val = []
+            while i < len(S) and S[i] != '-':
+                val.append(S[i])
+                i += 1
+            node = TreeNode(int("".join(val)))
+            if stack:
+                if stack[-1].left is None:
+                    stack[-1].left = node
+                else:
+                    stack[-1].right = node
+            stack.append(node)
+        return stack[0]
