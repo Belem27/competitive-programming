00f905a0aa0c103b8b3ab73d242bdb27cc36df67
JBelem <89881288+Belem27@users.noreply.github.com>
2024-07-06 23:52:47 +0000

    Created ' King Escape' by A2SV Hub

---
+++ 06-Jul-2024/ King Escape 177031.py
@@ -0,0 +1,10 @@
+# Problem:  King Escape - https://codeforces.com/problemset/problem/1033/A
+
+inp=int(input())
+ax,ay=(map(int,input().split()))
+bx,by=(map(int,input().split()))
+cx,cy=(map(int,input().split()))
+if (ax-cx)*(ax-bx)>0 and (ay-cy)*(ay-by)>0:
+    print('YES')
+else:
+    print('NO')
