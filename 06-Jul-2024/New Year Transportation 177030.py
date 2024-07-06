ff0f083e1904ec67a55e9f936efb842816ab84fe
JBelem <89881288+Belem27@users.noreply.github.com>
2024-07-06 23:49:23 +0000

    Created 'New Year Transportation' by A2SV Hub

---
+++ 06-Jul-2024/New Year Transportation 177030.py
@@ -0,0 +1,11 @@
+# Problem: New Year Transportation - https://codeforces.com/problemset/problem/500/A
+
+n, t = map(int, input().split())
+a = list(map(int, input().split()))
+i = 0
+t -= 1 
+
+while i < t:  
+    i += a[i]  
+
+print("YES" if i == t else "NO")
