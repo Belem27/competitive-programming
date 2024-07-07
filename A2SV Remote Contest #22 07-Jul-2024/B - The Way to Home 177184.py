39d5001d12db7820588aba404876b96279936e94
JBelem <89881288+Belem27@users.noreply.github.com>
2024-07-07 16:07:42 +0000

    Created 'B - The Way to Home' by A2SV Hub

---
+++ A2SV Remote Contest #22 07-Jul-2024/B - The Way to Home 177184.py
@@ -0,0 +1,30 @@
+# Problem: B - The Way to Home - https://codeforces.com/gym/533316/problem/B
+
+import sys
+
+def soln(n, d, s):
+    curr_pos = 0
+    jumps = 0
+    
+    while curr_pos < n - 1:
+        next_pos = curr_pos
+        for i in range(1, d + 1):
+            if curr_pos + i < n and s[curr_pos + i] == '1':
+                next_pos = curr_pos + i
+        
+        if next_pos == curr_pos:
+            return -1
+        
+        curr_pos = next_pos
+        jumps += 1
+        
+    return jumps
+
+input = sys.stdin.read
+data = input().split()
+n = int(data[0])
+d = int(data[1])
+s = data[2]
+
+if __name__ == "__main__":
+    print(soln(n, d, s))
