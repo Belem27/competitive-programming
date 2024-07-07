4660533247a06cadef2c0baae22c96b4a7f854cd
JBelem <89881288+Belem27@users.noreply.github.com>
2024-07-07 16:07:52 +0000

    Created 'A - Rudolph and Cut the Rope ' by A2SV Hub

---
+++ A2SV Remote Contest #22 07-Jul-2024/A - Rudolph and Cut the Rope  177185.py
@@ -0,0 +1,11 @@
+# Problem: A - Rudolph and Cut the Rope  - https://codeforces.com/gym/533316/problem/A
+
+for _ in range(int(input())):
+    n = int(input())
+    res = 0
+    for _ in range(n):
+        a,b = map(int, input().split())
+        if a > b:
+            res += 1
+    
+    print(res)
