8a31c54c4c5a749a8e98ca90a2012bd8c2400c72
JBelem <89881288+Belem27@users.noreply.github.com>
2024-07-07 16:07:30 +0000

    Created 'c - Reposts' by A2SV Hub

---
+++ A2SV Remote Contest #22 07-Jul-2024/c - Reposts 177183.py
@@ -0,0 +1,21 @@
+# Problem: c - Reposts - https://codeforces.com/gym/533316/problem/c
+
+import sys
+input = sys.stdin.read
+
+def soln(reposts):
+    repost_chain = {}
+    repost_chain["polycarp"] = 1
+
+    for repost in reposts:
+        name1, _, name2 = repost.lower().split()
+        repost_chain[name1] = repost_chain[name2] + 1
+    
+    return max(repost_chain.values())
+
+data = input().splitlines()
+n = int(data[0])
+reposts = data[1:]
+
+if __name__ == "__main__":
+    print(soln(reposts))
