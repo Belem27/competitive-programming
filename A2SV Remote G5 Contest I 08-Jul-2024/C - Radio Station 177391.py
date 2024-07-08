dd0150f9ae909742895b1930a57ffd7465834cd5
JBelem <89881288+Belem27@users.noreply.github.com>
2024-07-08 08:49:06 +0000

    Created 'C - Radio Station' by A2SV Hub

---
+++ A2SV Remote G5 Contest I 08-Jul-2024/C - Radio Station 177391.py
@@ -0,0 +1,13 @@
+# Problem: C - Radio Station - https://codeforces.com/gym/500425/problem/C
+
+n, m = map(int, input().split())
+q = {}
+for _ in range(n):
+    name, server = input().split()
+    q[server] = name
+
+for _ in range(m):
+    command, server = input().split()
+    ip = server[:-1]
+    if ip in q:
+        print(f"{command} {server} #{q[ip]}")
