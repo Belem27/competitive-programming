48778b756b5f9b3bf5f55377befced9c62fd4163
JBelem <89881288+Belem27@users.noreply.github.com>
2024-07-13 22:25:26 +0000

    Created 'B - Guess the Maximum' by A2SV Hub

---
+++ A2SV Remote Contest #23 13-Jul-2024/B - Guess the Maximum 180244.py
@@ -0,0 +1,9 @@
+# Problem: B - Guess the Maximum - https://codeforces.com/gym/535309/problem/B
+
+for i in range(int(input())):
+	n = int(input())
+	a = list(map(int,input().split()))
+	k = []
+	for i in range(n-1):
+		k.append(max(a[i],a[i+1]))
+	print(min(k)-1)
