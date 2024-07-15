c83d030633eb78507f0f438ba798cff9a7a4e8e0
JBelem <89881288+Belem27@users.noreply.github.com>
2024-07-15 22:40:09 +0000

    Created 'D - Heap Operations' by A2SV Hub

---
+++ A2SV Remote Contest #23 15-Jul-2024/D - Heap Operations 180744.py
@@ -0,0 +1,53 @@
+# Problem: D - Heap Operations - https://codeforces.com/gym/535309/problem/D
+
+from sys import stdin
+from heapq import heapify, heappop, heappush
+
+def input():
+    return stdin.readline().strip()
+
+t = int(input())
+heap = []
+heapify(heap)
+ans = []
+
+def insert(num):
+    heappush(heap, num)
+    ans.append(f"insert {num}")
+    return
+
+def getMin(num):
+    while len(heap) >= 1 and heap[0] < num:
+        remove()
+    if heap and heap[0] > num:
+        insert(num)
+    if not heap:
+        insert(num)
+    ans.append(f"getMin {num}")
+    return
+
+def remove():
+    if heap:
+        heappop(heap)
+        ans.append("removeMin")
+    else:
+        insert(0)
+        remove()
+    return
+
+
+for _ in range(t):
+    inputs = list(input().split())
+    if len(inputs) > 1:
+        func = inputs[0]
+        num = int(inputs[1])
+        if func == "insert":
+            insert(num)
+        else:
+            getMin(num)
+    else:
+        remove()
+
+print(len(ans))
+for i in range(len(ans)):
+    print(ans[i])
