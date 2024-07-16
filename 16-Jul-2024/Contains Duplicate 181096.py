f6f5f5a6c2a46875544a6ba4181ca3fbc4ae076a
JBelem <89881288+Belem27@users.noreply.github.com>
2024-07-16 22:48:09 +0000

    Created 'Contains Duplicate' by A2SV Hub

---
+++ 16-Jul-2024/Contains Duplicate 181096.py
@@ -0,0 +1,9 @@
+# Problem: Contains Duplicate - https://leetcode.com/problems/contains-duplicate/description/
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return True if len(set(nums)) < len(nums) else False
