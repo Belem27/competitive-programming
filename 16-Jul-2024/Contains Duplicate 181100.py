affb46072610ef48ecce6e57e6108ced263aac3d
JBelem <89881288+Belem27@users.noreply.github.com>
2024-07-16 22:53:50 +0000

    Created 'Contains Duplicate' by A2SV Hub

---
+++ 16-Jul-2024/Contains Duplicate 181100.py
@@ -0,0 +1,13 @@
+# Problem: Contains Duplicate - https://leetcode.com/problems/contains-duplicate/description/
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        nums.sort()
+        for i in range(1,len(nums)):
+            if nums[i-1] == nums[i]:
+                return True
+        return False
