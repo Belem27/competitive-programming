61276d40c47d10bf82df25bb8d18a68357e01027
JBelem <89881288+Belem27@users.noreply.github.com>
2024-07-20 16:04:00 +0000

    Created 'Insert Delete GetRandom O(1)' by A2SV Hub

---
+++ 20-Jul-2024/Insert Delete GetRandom O(1) 182145.py
@@ -0,0 +1,39 @@
+# Problem: Insert Delete GetRandom O(1) - https://leetcode.com/problems/insert-delete-getrandom-o1/
+
+class RandomizedSet(object):
+
+    def __init__(self):
+        self.hashmap = set()
+
+    def insert(self, val):
+        """
+        :type val: int
+        :rtype: bool
+        """
+        if val in self.hashmap:
+            return False
+        self.hashmap.add(val)
+        return True
+
+    def remove(self, val):
+        """
+        :type val: int
+        :rtype: bool
+        """
+        if val not in self.hashmap:
+            return False
+        self.hashmap.remove(val)
+        return True
+
+    def getRandom(self):
+        """
+        :rtype: int
+        """
+        return random.choice(list(self.hashmap))
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
