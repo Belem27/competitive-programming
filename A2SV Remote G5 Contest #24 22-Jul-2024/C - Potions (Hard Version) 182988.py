aba980574a98276e6c53d61db0201abb75481177
JBelem <89881288+Belem27@users.noreply.github.com>
2024-07-22 19:44:34 +0000

    Created 'C - Potions (Hard Version)' by A2SV Hub

---
+++ A2SV Remote G5 Contest #24 22-Jul-2024/C - Potions (Hard Version) 182988.py
@@ -0,0 +1,17 @@
+# Problem: C - Potions (Hard Version) - https://codeforces.com/gym/536373/problem/C
+
+import heapq
+
+n = int(input())
+potions = list(map(int, input().split()))
+
+heap = []
+health = 0
+
+for num in potions:
+    health += num
+    heapq.heappush(heap, num)
+    if health < 0:
+        health -= heapq.heappop(heap)
+
+print(len(heap))
