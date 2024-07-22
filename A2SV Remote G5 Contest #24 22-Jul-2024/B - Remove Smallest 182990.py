2a6a5ca46df7730061ffb6e796bfe4c390546efe
JBelem <89881288+Belem27@users.noreply.github.com>
2024-07-22 19:44:46 +0000

    Created 'B - Remove Smallest' by A2SV Hub

---
+++ A2SV Remote G5 Contest #24 22-Jul-2024/B - Remove Smallest 182990.py
@@ -0,0 +1,18 @@
+# Problem: B - Remove Smallest - https://codeforces.com/gym/536373/problem/B
+
+def soln(n, a):
+
+    a.sort()
+    possible = True
+    for i in range(n - 1):
+        if a[i + 1] - a[i] > 1:
+            possible = False
+            break
+
+    
+    return "YES" if possible else "NO"
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    print(soln(n, a))
