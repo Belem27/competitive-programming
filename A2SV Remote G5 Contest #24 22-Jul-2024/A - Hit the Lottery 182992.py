e714b13af9c31d024d4b7178c850979f47761041
JBelem <89881288+Belem27@users.noreply.github.com>
2024-07-22 19:45:03 +0000

    Created 'A - Hit the Lottery' by A2SV Hub

---
+++ A2SV Remote G5 Contest #24 22-Jul-2024/A - Hit the Lottery 182992.py
@@ -0,0 +1,18 @@
+# Problem: A - Hit the Lottery - https://codeforces.com/gym/536373/problem/A
+
+def soln(n):
+    bills_100 = n // 100
+    n = n % 100
+    bills_20 = n // 20
+    n = n % 20
+    bills_10 = n // 10
+    n = n % 10
+    bills_5 = n // 5
+    n = n % 5
+    bills_1 = n
+    
+    total_bills = bills_100 + bills_20 + bills_10 + bills_5 + bills_1
+    return total_bills
+
+n = int(input())
+print(soln(n))
