c9069a4149f65bc1b53d559afc0bd476cb3bc4a7
JBelem <89881288+Belem27@users.noreply.github.com>
2024-07-24 23:29:02 +0000

    Time: 366 ms (81.25%) | Memory: 20 MB (34.38%) - LeetSync

---
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def maximumElementAfterDecrementingAndRearranging(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        arr.sort()
+        result = 1
+        for i in xrange(1, len(arr)):
+            result = min(result+1, arr[i])
+        return result
