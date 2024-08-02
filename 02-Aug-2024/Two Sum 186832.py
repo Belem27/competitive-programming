f0554e3571f3a1c32cc7028fcd2edf27228d5586
JBelem <89881288+Belem27@users.noreply.github.com>
2024-08-02 18:14:59 +0000

    Created 'Two Sum' by A2SV Hub

---
+++ 02-Aug-2024/Two Sum 186832.py
@@ -0,0 +1,15 @@
+# Problem: Two Sum - https://leetcode.com/problems/two-sum/
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            if target - nums[i] in nums and nums.index(target - nums[i]) != i:
+                return [i, nums.index(target - nums[i])]
+        
+        return None
+                
