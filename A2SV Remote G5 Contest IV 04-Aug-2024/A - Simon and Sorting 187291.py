f56c5cb001743067da4001baa02d5bb1e1fb5bed
JBelem <89881288+Belem27@users.noreply.github.com>
2024-08-04 17:38:01 +0000

    Created 'A - Simon and Sorting' by A2SV Hub

---
+++ A2SV Remote G5 Contest IV 04-Aug-2024/A - Simon and Sorting 187291.py
@@ -0,0 +1,7 @@
+# Problem: A - Simon and Sorting - https://codeforces.com/gym/507024/problem/A
+
+for _ in range(int(input())):
+    if input() in {'bca', 'cab'}:
+        print("NO")
+    else:
+        print("YES")
