9e8e81be2170866de5aa200e8338474c04ea2865
JBelem <89881288+Belem27@users.noreply.github.com>
2024-08-04 17:37:54 +0000

    Created 'B - Not Equal Half' by A2SV Hub

---
+++ A2SV Remote G5 Contest IV 04-Aug-2024/B - Not Equal Half 187290.py
@@ -0,0 +1,9 @@
+# Problem: B - Not Equal Half - https://codeforces.com/gym/507024/problem/B
+
+n = int(input())
+a = list(map(int, input().split()))
+a.sort()
+if a[:n] != a[n:]:
+    print(*a)
+else:
+    print(-1)
