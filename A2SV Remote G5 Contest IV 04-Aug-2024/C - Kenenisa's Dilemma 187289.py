fcdf5374f16eabd99b9b6d432dd081ccbe28e7ae
JBelem <89881288+Belem27@users.noreply.github.com>
2024-08-04 17:37:41 +0000

    Created 'C - Kenenisa's Dilemma' by A2SV Hub

---
+++ A2SV Remote G5 Contest IV 04-Aug-2024/C - Kenenisa's Dilemma 187289.py
@@ -0,0 +1,26 @@
+# Problem: C - Kenenisa's Dilemma - https://codeforces.com/gym/507024/problem/C
+
+n = int(input())
+arr = list(map(int, input().split()))
+sort_arr = sorted(arr)
+
+if sort_arr == arr:
+    print(0)
+else:
+    count = 0
+    swaps = []
+    for i in range(n):
+        min_index = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        
+        if min_index != i:
+            count += 1
+            swaps.append(str(i) + " " + str(min_index))
+        
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+        
+    print(count)
+    print('\n'.join(swaps))
+        
