bb64d7081091f8620a95834bac70d033393fa9ae
JBelem <89881288+Belem27@users.noreply.github.com>
2024-08-07 18:47:03 +0000

    Time: 127 ms (66.56%) | Memory: 12.2 MB (19.29%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isBipartite(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: bool
+        """
+        side = [0] * len(graph)
+        def dfs(node):
+            for v in graph[node]:
+                if side[v] == 0: 
+                    side[v] = -side[node]
+                    if not dfs(v): 
+                        return False
+                elif side[v] == side[node]: 
+                    return False
+            return True
+        for i in range(len(graph)):
+            if side[i] == 0: 
+                side[i] = 1
+            if not dfs(i): 
+                return False
+        return True
