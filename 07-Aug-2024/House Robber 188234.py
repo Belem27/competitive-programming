2363f3568b3b131be4cb54ea4fdc00800d654020
JBelem <89881288+Belem27@users.noreply.github.com>
2024-08-07 18:42:08 +0000

    Created 'House Robber' by A2SV Hub

---
+++ 07-Aug-2024/House Robber 188234.py
@@ -0,0 +1,14 @@
+# Problem: House Robber - https://leetcode.com/problems/house-robber/
+
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) <= 2: 
+            return max(nums or [0])
+        nums[2] += nums[0]
+        for i in range(3, len(nums)): 
+            nums[i] += max(nums[i - 2], nums[i - 3])
+        return max(nums[-1], nums[-2])
