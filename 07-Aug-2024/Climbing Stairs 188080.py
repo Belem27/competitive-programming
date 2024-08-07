e9de224333b6abeb7db44767fc23fd6367cbc47c
JBelem <89881288+Belem27@users.noreply.github.com>
2024-08-07 03:30:29 +0000

    Created 'Climbing Stairs' by A2SV Hub

---
+++ 07-Aug-2024/Climbing Stairs 188080.py
@@ -0,0 +1,15 @@
+# Problem: Climbing Stairs - https://leetcode.com/problems/climbing-stairs/
+
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        one_step, two_step = 1,1
+
+        for i in range(n-1):
+            temp = one_step
+            one_step = one_step + two_step
+            two_step = temp
+        return one_step
