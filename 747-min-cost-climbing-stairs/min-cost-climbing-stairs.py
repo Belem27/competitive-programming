ddf678156d689e341a2a2799c5c32be28ef564ab
JBelem <89881288+Belem27@users.noreply.github.com>
2024-08-07 04:33:25 +0000

    Time: 28 ms (92.20%) | Memory: 11.7 MB (79.87%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -4,7 +4,6 @@ class Solution(object):
         :type cost: List[int]
         :rtype: int
         """
-        cost.append(0)
-        for i in range(len(cost)-3,-1,-1):
-            cost[i] += min(cost[i+2], cost[i+1])
-        return min(cost[0], cost[1])
+        for i in range(2, len(cost)):
+            cost[i] += min(cost[i-2], cost[i-1])
+        return min(cost[-2], cost[-1])
