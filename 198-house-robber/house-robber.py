bbf15ed183c7de85b0a61ef5731d08ac5c2b3d03
JBelem <89881288+Belem27@users.noreply.github.com>
2024-08-07 18:01:36 +0000

    Time: 16 ms (50.52%) | Memory: 11.7 MB (32.59%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in range(2,len(nums)):
+            nums[i] = max(nums[:i-1]) + nums[i]
+        print(nums)
+        return max(nums)
